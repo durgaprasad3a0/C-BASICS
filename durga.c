dbce3aa628c545c626460289a7b6eb2ed847fe22
durgaprasad3a0 <92911401+durgaprasad3a0@users.noreply.github.com>
2021-10-28 23:50:58 -0700

    Add files via upload

---
+++ durga.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+	//variable declaration
+	int age;
+	float cgpa;
+	char initial;
+	
+	//initialization
+	age = 17;
+	cgpa = 8.0;
+	initial = 'M';
+	
+	//output
+	printf("%d\n", age);
+	printf("%f\n", cgpa);
+	printf("%c", initial);
+}

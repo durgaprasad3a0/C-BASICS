109fad59e5cd6b40a54d58b6a8c6ccc5a190be1c
durgaprasad3a0 <92911401+durgaprasad3a0@users.noreply.github.com>
2021-12-11 12:57:34 +0600

    Add files via upload

---
+++ positive.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	int number;
+	printf("enter the number");
+	scanf("%d",&number);
+	if(number>0)
+	printf("%d is postivie number",number);
+	else if(number=0)
+	printf("&d is a integer number",number);
+	else
+	printf("%d is negative number",number);
+}
---
+++ vote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	int age;
+	printf("enter your age=");
+	scanf("%d",&age);
+	if (age>=18)
+	printf("your eligible for vote");
+	else
+	printf("your not eligible for vote");
+}
